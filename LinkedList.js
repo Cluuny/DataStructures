f12d0bb516edc4b44f7261c5467b6352056de407
Cluuny <vicente.matallana@uptc.edu.co>
2023-04-08 21:46:05 -0500

    Primera estructura hecha, Listas enlazadas simples

---
+++ LinkedList.js
@@ -0,0 +1,87 @@
+export class Node {
+	constructor(value) {
+		this.value = value;
+		this.index = undefined;
+		this.next = null;
+	}
+}
+export class SimpleLinkedList {
+	constructor() {
+		this.head = null;
+	}
+	nodeAt(index = undefined) {
+		let current = this.head;
+		while (index && index-- && current.next) {
+			current = current.next;
+		}
+		return current;
+	}
+	getNodeWith(value) {
+		let current = this.head;
+		while (current.value !== value) {
+			current = current.next;
+		}
+		return current;
+	}
+	add(value) {
+		let index = 0;
+		if (this.head === null) {
+			this.head = new Node(value);
+			this.head.index = index;
+		} else {
+			let current = this.head;
+			while (current.next !== null) {
+				++index;
+				current = current.next;
+			}
+			current.next = new Node(value);
+			current.next.index = ++index;
+		}
+		return true;
+	}
+	addAt(index = undefined, value) {
+		if (index === undefined) this.add(value);
+		else {
+			let newNode = new Node(value);
+			newNode.index = index;
+			let prevNode = this.nodeAt(index - 1);
+			let nextNode = this.nodeAt(index);
+			newNode.next = nextNode;
+			prevNode.next = newNode;
+			while (nextNode !== null) {
+				nextNode.index = ++index;
+				nextNode = nextNode.next;
+			}
+		}
+		return true;
+	}
+	replaceNode(index, value) {
+		let pastNode = this.nodeAt(index);
+		if (pastNode === null) return false;
+		else {
+			pastNode.value = value;
+			return true;
+		}
+	}
+	delete(index = undefined) {
+		if (index === undefined) {
+			let current = this.head;
+			while (current.next) {
+				current = current.next;
+			}
+			let previous = this.nodeAt(current.index - 1);
+			previous.next = null;
+			return true;
+		} else {
+			let prevNode = this.nodeAt(index - 1);
+			let actNode = prevNode.next;
+			let nextNode = actNode.next;
+			prevNode.next = nextNode;
+			while (nextNode) {
+				nextNode.index = --nextNode.index;
+				nextNode = nextNode.next;
+			}
+			return true;
+		}
+	}
+}
---
+++ index.js
@@ -0,0 +1,7 @@
+import { Node, SimpleLinkedList } from "./LinkedList.js";
+let simpleLinkedList = new SimpleLinkedList();
+simpleLinkedList.add(1);
+simpleLinkedList.add(2);
+simpleLinkedList.add(3);
+simpleLinkedList.add(4);
+console.log(simpleLinkedList);
